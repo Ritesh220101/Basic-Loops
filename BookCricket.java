a1b7e3c1bc5c651acd33b090e15272ecbbe86c10
Ritesh220101 <68066271+Ritesh220101@users.noreply.github.com>
2022-06-16 12:51:13 +0530

    Add files via upload

---
+++ BookCricket.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class BookCricket{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Player 1 Name: ");
+		String playerName1 = sc.next();
+		System.out.print("Enter Player 2 Name: ");
+		String playerName2 = sc.next();
+		
+		System.out.println("Player: "+playerName1);
+		System.out.println("Open the book");
+		int pageNumber1 = sc.nextInt();
+		int score1 = pageNumber1%7;
+		
+		while(pageNumber1%7!=0){
+			System.out.println("Open the book");
+			pageNumber1 = sc.nextInt();
+			score1 += pageNumber1%7;
+		}
+		System.out.println(playerName1+" score is: "+score1);
+		
+		System.out.println("Player: "+playerName2);
+		System.out.println("Open the book");
+		int pageNumber2 = sc.nextInt();
+		int score2 = pageNumber2%7;
+		
+		while(score2<score1 && pageNumber2%7!=0){
+			System.out.println("Open the book");
+			pageNumber2 = sc.nextInt();
+			score2 += pageNumber2%7;
+ 		}
+		System.out.println(playerName2+" score is: "+score2);
+		
+		if(score1>score2)
+			System.out.println(playerName1+" wins");
+		else System.out.println(playerName2+" wins"); 
+	}
+
+}
