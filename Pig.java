91f475033ae680546597f8b55be5e89acd7ec4b5
Ritesh220101 <68066271+Ritesh220101@users.noreply.github.com>
2022-06-20 22:01:06 +0530

    Add files via upload

--- Pig.java
+++ Pig.java
@@ -1,42 +1,47 @@
 package com.aurionpro.test;
+
 import java.util.*;
+
 public class Pig {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Random rand = new Random();
-		int totalSum  = 0;
+		int totalSum = 0;
 		int lower = 1;
 		int upper = 6;
 		int turns = 1;
 		int target = 20;
-		while(totalSum<target) {
-			System.out.println("TURN "+turns);
+		while (totalSum < target) {
+			System.out.println("TURN " + turns);
 			System.out.print("Roll or hold? (r/h): ");
 			char c = sc.next().charAt(0);
-			if(c=='r') {
-				
+			if (c == 'r') {
+
 				int die = (int) (Math.random() * (upper - lower)) + lower;
-				System.out.println("Die : "+die);
-				totalSum+=die;
-				
-				if(die==1) {
+				System.out.println("Die : " + die);
+				totalSum += die;
+
+				if (die == 1) {
 					totalSum = 0;
 					turns += 1;
-					System.out.println("Total Sum: "+totalSum);
+					System.out.println("Total Sum: " + totalSum);
 					System.out.println("Turn Over, No Score");
+					System.out.println();
 				}
-			}
-			else if(c=='h') {
-				turns+=1;
-				
-				System.out.println("Total sum :"+totalSum);
-				
+			} else if (c == 'h') {
+				turns += 1;
+
+				System.out.println("Total sum is: " + totalSum);
+				System.out.println();
+			} else {
+				System.out.println("Code Exited");
+				break;
 			}
 		}
-		if(totalSum >= target)
-			System.out.println("You finished in "+turns+" turns!");
-		
+		if (totalSum >= target) {
+			System.out.println("Total Sum is: " + totalSum);
+			System.out.println("\nYou finished in " + turns + " turns!");
+		}
 	}
-	
+
 }
