19208d59fc5a9feac004c35173276a3c9c927af8
Ritesh220101 <68066271+Ritesh220101@users.noreply.github.com>
2022-06-30 20:29:25 +0530

    Update Tictactoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -71,11 +71,11 @@ public class Tictactoe {
 	}
 
 	private static void printTicTacToe(String[][] a) {
-		System.out.println(a[0][0] + "|" + a[0][1] + "|" + a[0][2]);
-		System.out.println("--------");
-		System.out.println(a[1][0] + "|" + a[1][1] + "|" + a[1][2]);
-		System.out.println("--------");
-		System.out.println(a[2][0] + "|" + a[2][1] + "|" + a[2][2]);
+		System.out.println(" "+a[0][0]+" " + "|" +" "+ a[0][1]+" " + "|" + " "+a[0][2]+" ");
+		System.out.println("--------------");
+		System.out.println(" "+a[1][0]+" " + "|" +" "+ a[1][1]+" " + "|" + " "+a[1][2]+" ");
+		System.out.println("--------------");
+		System.out.println(" "+a[2][0]+" " + "|" +" "+ a[2][1]+" " + "|" + " "+a[2][2]+" ");
 	}
 
 }
